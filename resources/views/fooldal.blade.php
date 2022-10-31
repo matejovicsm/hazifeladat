822a9b215ce09b03b7c2c74640a9481776f9ea2c
matejovicsm <mate.matejovics@gmail.com>
2022-10-31 10:48:37 +0100

    főoldalon létrehoztunk beviteli mezőket

--- resources/views/fooldal.blade.php
+++ resources/views/fooldal.blade.php
@@ -2,10 +2,27 @@
 @section('title',"fooldal")
     
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row">
         <div class="col-6">
+            <div class="bg-dark text-white rounded p-3">
+                <form method="POST">
+                @csrf
+                    <div class="my-3">
+                        <label for="vezeteknev">Vezetéknév:</label>
+                        <input type="text" id="vezeteknev" name="vezeteknev" class="form-control">
+                    </div>
+                    <div class="my-3">
+                        <label for="keresztnev">Keresztnév:</label>
+                        <input type="text" id="keresztnev" name="keresztnev" class="form-control">
+                    </div>
+                    <div class="my-3">
+                        <label for="szuletesi_ev">Születési év:</label>
+                        <input type="year" id="szuletesi_ev" name="szuletesi_ev" class="form-control">
+                    </div>e
 
+                </form>
+            </div>
         </div>
 
     </div>
