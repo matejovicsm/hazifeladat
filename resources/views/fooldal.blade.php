5ff6340c6f19773734595f2e42ea65bad3c2ea27
matejovicsm <mate.matejovics@gmail.com>
2022-10-31 11:43:04 +0100

    az alert üzeneteket hozzáadtuk a fooldalhoz így mostmár megjelennek

--- resources/views/fooldal.blade.php
+++ resources/views/fooldal.blade.php
@@ -11,14 +11,32 @@
                     <div class="my-3">
                         <label for="vezeteknev">Vezetéknév:</label>
                         <input type="text" id="vezeteknev" name="vezeteknev" class="form-control">
+                        @error('vezeteknev')
+                            <div class="alert alert-danger">
+
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
                     <div class="my-3">
                         <label for="keresztnev">Keresztnév:</label>
                         <input type="text" id="keresztnev" name="keresztnev" class="form-control">
+                        @error('keresztnev')
+                            <div class="alert alert-danger">
+
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
                     <div class="my-3">
                         <label for="szuletesi_ev">Születési év:</label>
                         <input type="year" id="szuletesi_ev" name="szuletesi_ev" class="form-control">
+                        @error('szuletesi_ev')
+                            <div class="alert alert-danger">
+
+                                {{$message}}
+                            </div>
+                        @enderror
                     </div>
                     <div class="my-3">
                         
